5e163f17e01e5aa2dee693bb6838df3b1fbe8e01
Erick Humberto Cordova Gavilanes <erihucor@espol.edu.ec>
2019-03-27 22:42:53 -0500

    metodos de estudio

---
+++ Tecnicas de estudio.py
@@ -0,0 +1,118 @@
+Fuente: Curso de Preparación para el ENARM – Curso Dr. Prieto
+¿QUE ES ESTUDIAR?
+
+ Estudiar es un trabajo profesional por un lado y por otro es un arte. Un arte entendido como el dominio de una serie de destrezas, habilidades y técnicas, que se aprenden con el ejercicio y que permiten la consecución del objetivo propuesto, en este caso el estudio.
+
+ Estudiar de manera eficaz, esto significa con el mínimo esfuerzo y el máximo resultado.
+
+Estudiar, significa saber cómo hay que pensar, observar, concentrarse, organizar y analizar, en suma ser mentalmente eficiente. 
+
+LO NECESARIO PARA ESTUDIAR
+
+Lo elementos necesarios para estudiar son poder estudiar,querer estudiar y saber estudiar.
+
+Poder, se refiere a poseer las capacidades mínimas de inteligencia y de aptitudes.
+
+Querer, significa, tener motivación suficiente para el esfuerzo que supone el estudio.
+
+Saber, se refiere a dominar las técnicas de estudio y los hábitos necesarios para hacer más eficaz el trabajo de estudio.
+
+Por tanto, la actividad del estudio se integra en hábitos y métodos de estudio Es necesario tener en cuenta que el estudio es una destreza técnica y no hay destreza si no hay entrenamiento que le dé soporte.
+
+HÁBITOS DE ESTUDIO
+
+El estudio debe ser un hábito. “Hábito” significa una conducta que se repite. Por lo tanto el estudio debe ser diario. Es necesario estudiar todos los días, un rato, pero todos los días.
+
+DÓNDE Y CUÁNDO ESTUDIAR
+
+En una habitación que se disponga para ello, sin malos olores, bien aireada y ventilada, ni con mucho calor ni con frío, sin molestias de ruidos, ni otro tipo de distracciones (televisión, cuarto de juegos, radio, internet, teléfono inteligente). 
+
+Es recomendable  tener una mesa de trabajo amplia, en la que se pueda tener encima todo el material que se va a necesitar, sin otros objetos de distracción.
+
+La luz que sea preferiblemente natural y si no es así que sea blanca o azul y que proceda del lado contrario a la mano con  la que se escribe.
+
+Tanto la mesa como la silla deberán estar en consonancia con tu altura, ser cómodas pero no en exceso
+
+Con el tronco estirado y la espalda apoyada en el respaldo de la silla.
+
+Es muy importante estar decidido a la hora de ponerse a estudiar, no titubee e intente mentalizarse de que ese es el trabajo que se tiene que hacer y es mejor hacerlo  con alegría y distensión que enfadado y sin ganas.
+
+También es muy importante que sepa que es lo que va  a hacer cada día y tenerlo todo planificado (pero con flexibilidad).d Por eso es muy conveniente que haga un horario de estudio diario pero que tiene que ser realista, teniendo en cuenta sus capacidades, que sea realizable, factible.
+
+Recuerde que cada 50 minutos debe descansar  10 minutos. No prolongue ni un minuto los periodos de descanso que se fije.
+
+Siempre empiece por las asignaturas más fáciles para pasar posteriormente a las más difíciles y terminar con otras también fáciles. Las horas de  ocio también son importantes, no se olvide de ellas. Nunca debe dormir menos de 6 horas, es fundamental para luego rendir bien. En lo personal no recomiendo nunca estudiar de noche.
+
+PENSAMIENTO ANALÍTICO
+
+El pensamiento analítico es un proceso en que se registran
+tres aspectos esenciales:
+
+1. Leer, para separar las ideas que integran un tema
+2.	Jerarquizar ideas. Significa otorgar a cada idea un valor determinado. Qué es lo más importante, un ejemplo o la introducción. Si la idea que localizamos tiene valor, la escribiremos y haremos la sinopsis
+3.	Sinopsis, o cuadro sinóptico. Es la forma material de reproducir el pensamiento analítico
+
+SÍNTESIS
+
+Significa reducir un concepto a su  expresión  mínima.  Para ello es necesario seleccionar una palabra que represente un concepto.
+
+Pero siempre será necesario tener un gráfico que soporte el proceso de comprensión visualizado en una página.
+
+ETAPAS DEL MÉTODO DE ESTUDIO
+
+1.	Prelectura: Es una lectura ágil, breve y en corto tiempo,   no debe de llevar más de 4 o 5 minutos. Se trata de que en esta prelectura incorporemos información previa que tenemos  del  tema
+2.	Lectura analítica: Es una lectura profunda, se hará pensando. El tiempo que le dedicaremos será ilimitado.Iremos leyendo, analizando y realizando el gráfico que elegimos hacer.
+3.	Lectura de revisión: Significa volver a leer el tema (segunda lectura), con la finalidad de corrección del gráfico (cuadro sinóptico o diagrama de palabras clave),  se irá checando el tema con el gráfico. Iremos quitando palabras, o la completaremos.
+4.	Repetición activa: En este momento cerraremos el texto, sólo nos quedaremos con el cuadro o gráfico que elaboramos. Y en voz alta trataremos de reconstruir el tema de estudio
+5.	Memorización: Significa habilidad de memorizar estrictamente el cuadro sinóptico 
+6.	Un repaso, pero ahora sin consultar el cuadro sinóptico o diagrama,El primer repaso fija la información si se realiza en las primeras 8 horas después de haber estudiado
+
+MEMORIA
+
+Nuestra memoria es como un gran armario o archivador en el que es evidente que encontraremos mejor las cosas si las tenemos ordenadas de una forma lógica.
+
+La memoria y el proceso memorístico están formados por tres fases o etapas:
+
+•	Registrar: En esta fase adquirimos el contacto con los elementos que posteriormente memorizaremos,Pero es fundamental que lo que leamos y escribamos lo hagamos de una forma ordenada, lógica y comprendiendo lo que se lee.
+•	Retener: Cuanta más atención prestemos a lo que intentemos memorizar, más fácil nos será retenerlo.Es fundamental  la  concentración, aislarnos de ruidos, olores, gente que pasa por la habitación, de la televisión e incluso en ciertas ocasiones de la música.Para retener las cosas, releer los textos solo con lo subrayado, releer   la fichas o anotaciones y, por supuesto, releer los temas.
+•	Recordar: Para ello, la manera de haberlo retenido (ordenado, con lógica, con notas, subrayados, esquemas) va a ser fundamental y también el interés que hayamos puesto en ello. 
+
+Se insiste mucho en la lógica, orden e intención, pero es que realmente son las piezas claves de la memoria.
+
+EL OLVIDO Y EL REPASO
+
+Los facilitadores del olvido son los siguientes:
+
+•	Memorizar sin comprender
+•	Falta de atención y concentración
+•	Falta de repasos o mala distribución de los repasos
+•	No conectar lo aprendido con lo que sabemos
+•	No utilizar lo aprendido
+•	Exceso de nerviosismo que hace que nos bloqueemos
+•	Poco interés en lo que aprendemos
+
+NUEVE REFLEXIONES SOBRE EL ESTUDIO
+
+1.	LA IMAGEN ES LA CLAVE DEL RECUERDO
+El truco, en lo que se refiere al estudio, es traducir los conceptos abstractos a imágenes. 
+
+2.	¡ATENCIÓN A LA ATENCIÓN!
+No estamos atentos. Y lo peor es que no nos damos cuenta; nuestra “desatención“ nos pasa desapercibida. Todo ello tiene un remedio, tan sencillo como difícil, prestar atención con todos los sentidos.
+
+3.	¡NO HAY APRENDIZAJE SIN ACTIVIDAD!
+Cuanto más activo, menos olvido. Se trata de “actividad“ mental, no física. Incrementar la actividad −en situación de “alumno“,? La respuesta es única: tomando notas. Al hacerlo se mantiene despierto
+
+4.	LA TOMA DE NOTAS ES UNA PRUEBA DE INTELIGENCIA
+Cuánto más tonto, más detallados serán los apuntes tomados en clase. Notas extensas = cabeza hueca. Las “notas“ deben ser breves e incompletas. 
+Se trata de utilizar esas pocas palabras registradas como estímulos para recordar todo el pensamiento expuesto.
+Una persona “inteligente“ no es aquella que “no comete errores“, sino alguien que aprende de sus errores.
+
+5.	¡EL REPASO ES LA CLAVE DEL EXAMEN!
+Notas breves incitan a repasarlas para completarlas (lo ideal, dentro de las 24 horas siguientes). Y ésta es otra cualidad de las notas breves: que no pueden dejarse sin completar. Sólo   el repaso consolida lo comprendido.
+Resumiendo, el primero al finalizar la clase; luego a las 24 horas, a la semana, al mes, a  los seis meses.
+
+6.	ESCRIBIR PARA PENSAR MEJOR
+Si alguien quiere aprender, que escriba.
+Escribir sencillo para pensar con claridad. Escribir  es “regar“ la planta del pensamiento reflexivo.
+
+7.	PARA SOBREVIVIR A SU EXAMEN NACIONAL ¡SEA ESQUEMÁTICO!
